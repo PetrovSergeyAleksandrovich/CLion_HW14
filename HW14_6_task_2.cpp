ad97ed8f32d1a73b454d57cc463d601f2943c565
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-01-30 00:15:42 +0300

    task 2 XO tmp commit

--- HW14_6_task_2.cpp
+++ HW14_6_task_2.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <string>
 
+int main()
+{
+    std::string str1, str2, str3;
+
+    std::cout << "1st line: ";
+    std::cout << std::endl;
+    std::getline(std::cin, str1);
+    std::cout << std::endl;
+
+    std::cout << "2st line: ";
+    std::getline(std::cin, str2);
+    std::cout << std::endl;
+
+    std::cout << "3st line: ";
+    std::getline(std::cin, str3);
+    std::cout << std::endl;
+
+    std::cout << str1 << "\n" << str2 << "\n" << str3 << "\n";
+
+    
+}
