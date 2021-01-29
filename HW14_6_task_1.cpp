791f2513c9cc312969af91085b8793631061a18c
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-01-29 23:42:05 +0300

    task 1 CaesarEncrypt done

--- HW14_6_task_1.cpp
+++ HW14_6_task_1.cpp
@@ -5,12 +5,9 @@ std::string encrypt(std::string input, int N)
 {
     for(int i = 0; i < input.length(); i++)
     {
-//        std::cout << "before: " << int(input[i]) << std::endl;
         int tmp = input[i] + N % 25;
         if(tmp > 122) tmp = 96 + tmp % 122;
         input[i] = tmp;
-//        std::cout << "after: " << int(input[i]) << std::endl;
-//        std::cout << std::endl;
     }
     return input;
 }
@@ -19,12 +16,9 @@ std::string decrypt(std::string input, int N)
 {
     for(int i = 0; i < input.length(); i++)
     {
-//        std::cout << "before: " << int(input[i]) << std::endl;
         int tmp = input[i] - N % 25;
         if(tmp < 97) tmp = 123 - 97 % tmp;
         input[i] = tmp;
-//        std::cout << "after: " << int(input[i]) << std::endl;
-//        std::cout << std::endl;
     }
     return input;
 }
@@ -37,18 +31,16 @@ int main()
     std::cout << "input: ";
     std::getline(std::cin, str);
 
-    std::cout << "\nYour input: " << str << std::endl;
-
     std::cout << "input encrypted number: ";
     std::cin >> n;
     str = encrypt(str, n);
-
-    std::cout << "\nYour input encrypted: " << str << std::endl;
+    std::cout << "\nencrypted: " << str << std::endl;
+    std::cout << std::endl;
 
     std::cout << "input decrypted number: ";
     std::cin >> n;
     str = decrypt(str, n);
-
-    std::cout << "\nYour input decrypted: " << str << std::endl;
+    std::cout << "\ndecrypted: " << str << std::endl;
+    std::cout << std::endl;
 
 }
