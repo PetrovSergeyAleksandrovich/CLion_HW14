39c8c7cc081185838ed1c52ecbf771bbd60a4b72
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-02-01 17:58:04 +0300

    task 5 check FLOATS done 2

--- HW14_6_task_5.cpp
+++ HW14_6_task_5.cpp
@@ -14,16 +14,19 @@ std::string input()
 
         std::cout << "\n\nType the number > ";
         std::cin >> n;
+
         //Counting the content of the string
         for(int i = 0; i < n.length(); i++){
             if (n[i] >= '0' && n[i] <= '9') digit += 1;
             if (n[i] == '-') minus += 1;
             if (n[i] == '.') dot += 1;
         }
+
         //Check if INPUT is OK
         if (minus>1 || dot>1) answer = false;
         else if (minus == 1 && n[0] != '-') answer = false;
         else if (digit == 0) answer = false;
+        
         //Print the answer
         if(answer) output = n;
         else{
---
+++ cmake-build-debug/CMakeFiles/task_5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task_5.dir/HW14_6_task_5.cpp.obj"
+  "task_5.exe"
+  "task_5.exe.manifest"
+  "task_5.lib"
+  "task_5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
